afeced8a5402506a63bd88cdb0f83244f06e034e
Dan Sinclair <dsinclair@horizontal.com>
2021-09-16 10:03:47 -0500

    Adding FileQuery GraphQL query

---
+++ src/JssPlayground.GraphQL/GraphTypes/FileGraphType.cs
@@ -0,0 +1,50 @@
+﻿using GraphQL.Types;
+using System.IO;
+
+namespace JssPlayground.GraphQL.GraphTypes
+{
+	public class FileGraphType : ObjectGraphType<FileSystemInfo>
+	{
+		public FileGraphType()
+		{
+			Name = "File";
+
+			Field<StringGraphType>("name", "Filename of the file not including extension", resolve: ResolveName);
+			Field<StringGraphType>("extension", "Extension of the file", resolve: ResolveExtension);
+			Field<StringGraphType>("path", "Full path of the file", resolve: ResolvePath);
+			Field<DateTimeGraphType>("created", "Created date and time of the file", resolve: ResolveCreated);
+			Field<DateTimeGraphType>("updated", "Updated date and time of the file", resolve: ResolveUpdated);
+			Field<DateTimeGraphType>("accessed", "Last accessed date and time of the file", resolve: ResolveAccessed);
+		}
+
+		private object ResolveAccessed(ResolveFieldContext<FileSystemInfo> arg)
+		{
+			return arg.Source.LastAccessTime;
+		}
+
+		private object ResolveUpdated(ResolveFieldContext<FileSystemInfo> arg)
+		{
+			return arg.Source.LastWriteTime;
+		}
+
+		private object ResolveCreated(ResolveFieldContext<FileSystemInfo> arg)
+		{
+			return arg.Source.CreationTime;
+		}
+
+		private object ResolvePath(ResolveFieldContext<FileSystemInfo> arg)
+		{
+			return arg.Source.FullName;
+		}
+
+		private object ResolveExtension(ResolveFieldContext<FileSystemInfo> arg)
+		{
+			return arg.Source.Extension;
+		}
+
+		private object ResolveName(ResolveFieldContext<FileSystemInfo> arg)
+		{
+			return arg.Source.Name;
+		}
+	}
+}
---
+++ src/JssPlayground.GraphQL/Queries/FileQuery.cs
@@ -0,0 +1,55 @@
+﻿using GraphQL.Types;
+using JssPlayground.GraphQL.GraphTypes;
+using Sitecore;
+using Sitecore.Data;
+using Sitecore.Services.GraphQL.Content;
+using Sitecore.Services.GraphQL.Schemas;
+using System;
+using System.IO;
+using System.Web;
+
+namespace JssPlayground.GraphQL.Queries
+{
+	public class FileQuery : RootFieldType<FileGraphType, FileSystemInfo>, IContentSchemaRootFieldType
+	{
+		public FileQuery() : base("file", "Supports querying for files in the filesystem")
+		{
+			base.Arguments = new QueryArguments
+			{
+				new QueryArgument<StringGraphType>
+				{
+					Name = "path"
+				}
+			};
+		}
+
+		public Database Database { get; set; }
+
+		protected override FileSystemInfo Resolve(ResolveFieldContext context)
+		{
+			string path = context.GetArgument<string>("path");
+
+			if (path == null)
+			{
+				throw new ArgumentNullException(nameof(path), "Must provide a path");
+			}
+
+			if (!path.StartsWith("/", StringComparison.Ordinal) && HttpContext.Current?.Server != null)
+			{
+				path = HttpContext.Current?.Server.MapPath(StringUtil.EnsurePrefix('/', path));
+			}
+
+			FileSystemInfo result = null;
+			if (Directory.Exists(path))
+			{
+				result = new DirectoryInfo(path);
+			}
+			else if (File.Exists(path))
+			{
+				result = new FileInfo(path);
+			}
+
+			return result;
+		}
+	}
+}
